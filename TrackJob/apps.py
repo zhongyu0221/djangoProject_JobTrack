dbfa585964921aa8e2f5961dca7f8759ad946167
zhongyu0221 <zhongyu0221@gmail.com>
2022-07-16 22:30:03 -0500

    Django
    asd

---
+++ TrackJob/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TrackjobConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'TrackJob'
---
+++ TrackJob/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Job(models.Model):
+
+
+    #Blank: How the field is rendered (F = Requied Field, NULL: DB)
+    Job_title = models.CharField(max_length=120) # required to have maxlength
+    Job_JD = models.TextField()
+    applied = models.BooleanField(default=True)
+    Submitted_Date = models.TextField(default='sss')
+    TakehomePoint = models.TextField(blank=False, null=True)
+    RequiredSkills = models.TextField(default='sss')
+
+
+
+

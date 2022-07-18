98e6a8f5165b756d1d3f8314caf61a10509b0946
zhongyu0221 <zhongyu0221@gmail.com>
2022-07-18 13:31:06 -0500

    Update models.py

--- TrackJob/models.py
+++ TrackJob/models.py
@@ -6,10 +6,10 @@ class Job(models.Model):
     #Blank: How the field is rendered (F = Requied Field, NULL: DB)
     Job_title = models.CharField(max_length=120) # required to have maxlength
     Job_JD = models.TextField()
-    applied = models.BooleanField(default=True)
-    Submitted_Date = models.TextField(default='sss')
+    #applied = models.BooleanField(default=True)
+    Submitted_Date = models.TextField(default='Replace with a Date')
     TakehomePoint = models.TextField(blank=False, null=True)
-    RequiredSkills = models.TextField(default='sss')
+    RequiredSkills = models.TextField(default='Replace with Coding Skills')
 
 
 
